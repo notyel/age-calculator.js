548698733c51f5cba2307ccd7080ae1c9768d192
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2024-05-29 00:38:08 -0500

    Eliminar los directorios node_modules y dist del repositorio

--- dist/test/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// src/test.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-var ageCalculator_1 = require("../ageCalculator");
-console.log((0, ageCalculator_1.calculateAge)("1990-05-25")); // Debería imprimir la edad basada en la fecha actual
